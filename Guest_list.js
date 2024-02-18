6946afb865b0265e50c90f41c8a1a8af82d10e30
Muntaha-Noor <160227252+Muntaha-Noor@users.noreply.github.com>
2024-02-18 17:11:00 +0500

    Add files via upload

---
+++ Guest_list.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_List = ['Muntaha Noor', 'Warda Noor', 'Musfirah Noor'];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Miss.' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n');
+}
